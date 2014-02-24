b66eccdfebae37e5ae366f03c98dbf12af9fed17
bazhan <bazhan12@mail.ru>
2014-02-24 16:20:14 +0400

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase } #Перевод почты к нижнему регистру перед сохранением в БД
+	#before_save { email.downcase! } # Альтернативная реализация before_save
 	validates :name, presence: true , length: { maximum: 50}  #Проверка на наличие имени и длину(максиммум 50)
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
 	uniqueness: { case_sensitive: false} # Проверка на наличие почты, соответствие формату,уникальность и нечуствительность к регистру
 	
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Bazhan"
+    email    "bazhan12@mail.ru"
+    password "bazhan"
+    password_confirmation "bazhan"
+  end
+end
